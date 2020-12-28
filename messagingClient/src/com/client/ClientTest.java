398b498981aa00c0c77e8f62b3fb06609a7b1990
Ian Bastos <ian-bastos@live.com>
2020-12-28 21:58:14 +0000

    clean formatting

--- messagingClient/src/com/client/ClientTest.java
+++ messagingClient/src/com/client/ClientTest.java
@@ -7,8 +7,7 @@ public class ClientTest {
 	public static void main(String[] args) {
 		Client client = new Client("127.0.0.1");
 		client.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		client.startRunning();
-		
-		
+		client.startRunning();	
 	}
+	
 }
