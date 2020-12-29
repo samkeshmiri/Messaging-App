c589f484804788e7c042ad79280fda522624df2a
Ian Bastos <ian-bastos@live.com>
2020-12-29 02:55:53 +0000

    connection pooling and delegate client connection to echo thread

---
+++ messagingServer/src/com/sam/server/EchoThread.java
@@ -0,0 +1,45 @@
+package com.sam.server;
+
+public class EchoThread extends Thread {
+    protected Socket socket;
+
+    public EchoThread(Socket clientSocket) {
+        this.socket = clientSocket;
+    }
+
+    public void run() {
+        InputStream inp = null;
+        BufferedReader brinp = null;
+        DataOutputStream out = null;
+
+        try {
+            inp = socket.getInputStream();
+            // create a buffered reader for its utility methods and wrap it arounf our input stream
+            brinp = new BufferedReader(new InputStreamReader(inp));
+
+            // we need an output stream to echo back what they said
+            out = new DataOutputStream(socket.getOutputStream());
+        } catch (IOException e) {
+            return;
+        }
+
+        String line;
+
+        while (true) {
+            try {
+                line = brinp.readLine();
+                if ((line == null) || line.equalsIgnoreCase("QUIT")) {
+                    socket.close();
+                    return;
+                } else {
+                    // write out what they sent to the connection
+                    out.writeBytes(line + "\n\r");
+                    out.flush();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+                return;
+            }
+        }
+    }
+}
